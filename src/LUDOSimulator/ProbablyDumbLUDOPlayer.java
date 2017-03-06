e5e61b44c219c10d69d751d8d80c0c68f46b27e9
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-03-06 22:22:40 +0100

    Initial commit with the game and beginning of my AI called ProbablyDumbLUDOPlayer

---
+++ src/LUDOSimulator/ProbablyDumbLUDOPlayer.java
@@ -0,0 +1,51 @@
+package LUDOSimulator;
+import java.util.Random;
+/**
+ * My first AI player
+ * @author Daniel Alexander Nedergaard
+ * 
+ */
+
+public class ProbablyDumbLUDOPlayer implements LUDOPlayer
+{
+	LUDOBoard board;
+	Random random;
+	
+	public ProbablyDumbLUDOPlayer(LUDOBoard board)
+	{
+		this.board = board;
+		random = new Random();
+	}
+	
+	public void play()
+	{
+		board.print("Probably dumb player playing");
+		
+		int[] myBricksValue = new int[4];
+		board.rollDice();
+		float max =-1;
+		int bestIndex =-1;
+		for (int i = 0; i < myBricksValue.length; i++)
+		{
+			float value = analyseBrickPositions(i);
+			if(value > max && value > 0)
+			{
+				bestIndex = i;
+				max = value;
+			}
+		}
+		if (bestIndex != -1)
+			board.moveBrick(bestIndex);
+	}
+	
+	private float analyseBrickPositions(int i)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void miniMaxHeuristic()
+	{
+	
+	}
+}
